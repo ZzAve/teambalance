c48b48444e067e38cb6eb896b4fba0f594bd48ae
jvandis <jvandis@bol.com>
2020-08-12 23:15:58 +0200

    Sort attendees by the role they have, not the name.

--- src/main/kotlin/nl/jvandis/teambalance/api/attendees/AttendeeRepository.kt
+++ src/main/kotlin/nl/jvandis/teambalance/api/attendees/AttendeeRepository.kt
@@ -4,7 +4,7 @@ import org.springframework.data.domain.Sort
 import org.springframework.data.repository.CrudRepository
 
 interface AttendeeRepository : CrudRepository<Attendee, Long> {
-    fun findAllByEventIdIn(eventIds: List<Long>, sort: Sort = Sort.by("userName")): List<Attendee>
+    fun findAllByEventIdIn(eventIds: List<Long>, sort: Sort = Sort.by("userRole","userName")): List<Attendee>
     fun findAllByUserIdIn(userIds: List<Long>): List<Attendee>
     fun findALlByEventIdInAndUserIdIn(eventIds: List<Long>, userIds: List<Long>): List<Attendee>
     fun findByUserIdAndEventId(userId: Long, trainingId: Long): List<Attendee>
