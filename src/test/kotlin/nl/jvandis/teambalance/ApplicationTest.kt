7fc18f9d3cf911524e5ff86ffa750b4ed4f50f35
jvandis <jvandis@bol.com>
2020-10-10 15:34:12 +0200

    Use GCPs secret manager

---
+++ src/test/kotlin/nl/jvandis/teambalance/ApplicationTest.kt
@@ -0,0 +1,13 @@
+package nl.jvandis.teambalance
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
