e15e21801fb7843c777712fced31d98b79a1077c
Julius van Dis <zzave@duck.com>
2024-02-04 13:44:15 +0100

    WIP add frontend menu

--- frontend/src/main/react/src/components/TopBar.tsx
+++ frontend/src/main/react/src/components/TopBar.tsx
@@ -8,7 +8,28 @@ import { Refresh } from "./Refresh";
 import { Logout } from "./Logout";
 import { TeamBalanceTheme, TenantContext } from "../TenantContext";
 import { ThemeToggler } from "./ThemeToggler";
+import {
+  Box,
+  Divider,
+  IconButton,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  SwipeableDrawer,
+} from "@mui/material";
 
+import SavingsIcon from "@mui/icons-material/Savings";
+import MenuIcon from "@mui/icons-material/Menu";
+import GroupsIcon from "@mui/icons-material/Groups";
+import List from "@mui/material/List";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import EventIcon from "@mui/icons-material/Event";
+import ScoreboardIcon from "@mui/icons-material/Scoreboard";
+import SportsVolleyballIcon from "@mui/icons-material/SportsVolleyball";
+import AdminPanelSettingsIcon from "@mui/icons-material/AdminPanelSettings";
+import PaymentsIcon from "@mui/icons-material/Payments";
 const TopBar = (props: {
   handleRefresh: () => void;
   refresh: boolean;
@@ -30,6 +51,17 @@ const TopBar = (props: {
     <AppBar position="static">
       <Toolbar>
         <Grid container spacing={2} alignItems="center">
+          <Grid item>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <Menu />
+            </IconButton>
+          </Grid>
           <Grid item xs>
             <Typography variant="h6">
               {tenantContext.title} Team balance
@@ -52,6 +84,123 @@ const TopBar = (props: {
         </Grid>
       </Toolbar>
     </AppBar>
+  );
+};
+
+export const Menu = () => {
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  const toggleDrawer =
+    (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
+      if (
+        event &&
+        event.type === "keydown" &&
+        ((event as React.KeyboardEvent).key === "Tab" ||
+          (event as React.KeyboardEvent).key === "Shift")
+      ) {
+        return;
+      }
+
+      setIsOpen(open);
+    };
+
+  const menuList = () => (
+    <Box
+      sx={{ width: 250 }}
+      role="presentation"
+      onClick={toggleDrawer(false)}
+      onKeyDown={toggleDrawer(false)}
+    >
+      <List>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <SportsVolleyballIcon />
+            </ListItemIcon>
+            <ListItemText primary="Trainingen" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <ScoreboardIcon />
+            </ListItemIcon>
+            <ListItemText primary="Wedstrijden" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <EventIcon />
+            </ListItemIcon>
+            <ListItemText primary="Overige Events" />
+          </ListItemButton>
+        </ListItem>
+      </List>
+      <Divider />
+      <List>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <SavingsIcon />
+            </ListItemIcon>
+            <ListItemText primary="Balans" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <PaymentsIcon />
+            </ListItemIcon>
+            <ListItemText primary="Transacties" />
+          </ListItemButton>
+        </ListItem>
+      </List>
+      <Divider />
+      <List>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <DarkModeIcon />
+              <LightModeIcon />
+            </ListItemIcon>
+            <ListItemText primary="Toggle mode" />
+          </ListItemButton>
+        </ListItem>
+      </List>
+      <Divider />
+      <List>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <GroupsIcon />
+            </ListItemIcon>
+            <ListItemText primary="Team" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <AdminPanelSettingsIcon />
+            </ListItemIcon>
+            <ListItemText primary="Admin" />
+          </ListItemButton>
+        </ListItem>
+      </List>
+    </Box>
+  );
+  return (
+    <React.Fragment key="left">
+      <MenuIcon onClick={toggleDrawer(true)} />
+      <SwipeableDrawer
+        anchor="left"
+        open={isOpen}
+        onClose={toggleDrawer(false)}
+        onOpen={toggleDrawer(true)}
+      >
+        {menuList()}
+      </SwipeableDrawer>
+    </React.Fragment>
   );
 };
 
