8d7c67aedd0cd88820d9b4b7f6d535b58d331503
Julius van Dis <zzave@duck.com>
2025-09-21 18:58:58 +0200

    Refactor BunqRepo initialization for improved readability (#302)
    * Refactor BunqRepo methods for enhanced initialization and code readability
    
    Addresses !301
    
    * Refactor BunqRepo to improve SDK initialization, session management, and API request handling
    
    * Last changes. let's go!

--- backend/src/main/kotlin/nl/jvandis/teambalance/api/bank/BunqRepo.kt
+++ backend/src/main/kotlin/nl/jvandis/teambalance/api/bank/BunqRepo.kt
@@ -2,9 +2,9 @@ package nl.jvandis.teambalance.api.bank
 
 import com.bunq.sdk.BUNQ_PUBLIC_SERVER
 import com.bunq.sdk.BUNQ_SANDBOX_SERVER
+import com.bunq.sdk.BunqSigningKeysGenerator
 import com.bunq.sdk.Config
 import com.bunq.sdk.Context
-import com.bunq.sdk.Signing
 import com.bunq.sdk.generated.Sdk
 import com.bunq.sdk.generated.endpoint.List_all_MonetaryAccountBank_for_User
 import com.bunq.sdk.generated.endpoint.List_all_Payment_for_User_MonetaryAccount
@@ -15,13 +15,14 @@ import com.bunq.sdk.generated.model.MonetaryAccountBankListing
 import com.bunq.sdk.generated.model.PaymentListing
 import com.bunq.sdk.handler
 import com.bunq.sdk.initContext
+import com.bunq.sdk.refreshSession
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
 import nl.jvandis.teambalance.api.bank.BankConfig.BankBunqConfig
 import nl.jvandis.teambalance.api.bank.BankConfig.BunqEnvironment.PRODUCTION
 import nl.jvandis.teambalance.api.bank.BankConfig.BunqEnvironment.SANDBOX
 import nl.jvandis.teambalance.log
-import java.io.File
+import java.time.Instant
 import java.time.LocalDateTime
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
@@ -34,6 +35,7 @@ class BunqRepo(
     private val bunqConfig: BankBunqConfig,
 ) {
     // Lazily initialized SDK components
+    private lateinit var config: Config
     private lateinit var sdk: Sdk
     private lateinit var context: Context
 
@@ -43,7 +45,7 @@ class BunqRepo(
     // Ensure only a single SDK invocation at a time
     private val sdkMutex = Mutex()
 
-    // Ensure initialization happens once
+    // Ensure initialization happens only once
     private val initMutex = Mutex()
 
     private suspend fun ensureInitialized() {
@@ -54,10 +56,9 @@ class BunqRepo(
             log.info("Initializing Bunq SDK (lazy)")
             val initTiming =
                 measureTimeMillis {
-                    val config = getBunqSdkConfig()
-                    val signing = Signing(config)
-                    context = initContext(config = config)
-                    sdk = Sdk(handler(context = context, signing = signing))
+                    config = getBunqSdkConfig()
+                    context = initContext(config)
+                    sdk = Sdk(handler(context = context))
                 }
 
             log.info("Done initializing Bunq SDK in ${initTiming}ms")
@@ -65,11 +66,18 @@ class BunqRepo(
         }
     }
 
+    private fun ensureSessionActive() {
+        val session = context.sessionExpiryTime
+        if (session == null || session.minusSeconds(30).isBefore(Instant.now())) {
+            context = context.refreshSession()
+            sdk = Sdk(handler(context))
+        }
+    }
+
     suspend fun getAccountBalance(accountId: Long): String =
-        sdkMutex.withLock {
-            ensureInitialized()
+        withBunqContext {
             val monetaryAccountResponse =
-                sdk.rEAD_MonetaryAccountBank_for_User(
+                rEAD_MonetaryAccountBank_for_User(
                     userID = context.userId,
                     itemId = accountId,
                 )
@@ -90,10 +98,9 @@ class BunqRepo(
     private fun Amount.toDomain(): String = "${parseCurrency()} $value"
 
     suspend fun getTransactions(accountId: Long): List<Transaction> =
-        sdkMutex.withLock {
-            ensureInitialized()
+        withBunqContext {
             val paymentsResponse =
-                sdk.list_all_Payment_for_User_MonetaryAccount(
+                list_all_Payment_for_User_MonetaryAccount(
                     userID = context.userId,
                     monetaryaccountID = accountId,
                     count = 200,
@@ -113,10 +120,9 @@ class BunqRepo(
         }
 
     suspend fun listMonetaryAccountBank(): List<BankAccount> =
-        sdkMutex.withLock {
-            ensureInitialized()
+        withBunqContext {
             val bankAccountsResponse =
-                sdk.list_all_MonetaryAccountBank_for_User(
+                list_all_MonetaryAccountBank_for_User(
                     userID = context.userId,
                     count = null,
                     newer_id = null,
@@ -129,13 +135,34 @@ class BunqRepo(
 
                 is List_all_MonetaryAccountBank_for_User.Response400 -> TODO()
             }
+        }.also { bankAccounts ->
+            bankAccounts.forEach {
+                if (it.balance.endsWith("0.00")) {
+                    log.warn("Found account with 0 balance: ${it.alias} (${it.id}). Requesting spending money")
+                    requireSpendingMoney(context.userId, it.id, sdk)
+                }
+            }
         }
 
+    /**
+     * Help function that ensures interaction with the Bunq API is thread safe
+     */
+    private suspend fun <T> withBunqContext(block: suspend Sdk.() -> T): T {
+        ensureInitialized()
+        return sdkMutex.withLock {
+            ensureSessionActive()
+            with(sdk) {
+                block()
+            }
+        }
+    }
+
     private fun getBunqSdkConfig(): Config {
+        val signingKeys = BunqSigningKeysGenerator.generateBunqSigningKeys()
         val config =
             when (bunqConfig.environment) {
                 PRODUCTION -> {
-                    require(bunqConfig.apiKey != null) {
+                    require(!bunqConfig.apiKey.isNullOrBlank()) {
                         "Production environment requires an API key."
                     }
 
@@ -143,29 +170,34 @@ class BunqRepo(
                         bunqServer = BUNQ_PUBLIC_SERVER,
                         serviceName = "bunq-sdk-teambalance",
                         apiKey = bunqConfig.apiKey,
-                        publicKeyFile = File("./public_key.pem").also { it.deleteOnExit() },
-                        privateKeyFile = File("./private_key.pem").also { it.deleteOnExit() },
+                        signingKeys = signingKeys,
                     )
                 }
 
                 SANDBOX -> {
-                    require(bunqConfig.apiKey.isNullOrEmpty() || bunqConfig.apiKey.startsWith("sandbox")) {
-                        "Sandbox environment API keys always start with 'sandbox'. " +
-                            "Yours doesn't, and is most likely a misconfiguration."
-                    }
                     val apiKey =
-                        if (bunqConfig.apiKey?.startsWith("sandbox") == true) {
-                            bunqConfig.apiKey
-                        } else {
-                            createSandboxUserApiKey(BUNQ_SANDBOX_SERVER.baseUrl)
+                        when {
+                            bunqConfig.apiKey.isNullOrBlank() -> {
+                                createSandboxUserApiKey(BUNQ_SANDBOX_SERVER.baseUrl)
+                            }
+
+                            bunqConfig.apiKey.startsWith("sandbox") -> {
+                                bunqConfig.apiKey
+                            }
+
+                            else -> {
+                                log.warn(
+                                    "Provided API key does not start with 'sandbox'. Falling back to generated sandbox key.",
+                                )
+                                createSandboxUserApiKey(BUNQ_SANDBOX_SERVER.baseUrl)
+                            }
                         }
 
                     Config(
                         bunqServer = BUNQ_SANDBOX_SERVER,
                         serviceName = "bunq-sdk-teambalance",
                         apiKey = apiKey,
-                        publicKeyFile = File("./public_key.pem").also { it.deleteOnExit() },
-                        privateKeyFile = File("./private_key.pem").also { it.deleteOnExit() },
+                        signingKeys = signingKeys,
                     )
                 }
             }
@@ -176,13 +208,16 @@ class BunqRepo(
 private fun MonetaryAccountBankListing.toDomain() =
     BankAccount(
         id = id ?: -1,
-        balance = balance?.parseCurrency() ?: "Unknown",
+        balance = balance.toDomain(),
         alias = alias?.filter { it.name != null }?.joinToString { it.name!! },
     )
 
+private fun Amount?.toDomain(): String = if (this == null) "Unknown" else "${parseCurrency()} $value"
+
 private fun PaymentListing.toDomain(): Transaction {
     require(amount != null && created != null && counterparty_alias.isValid()) {
-        "PaymentListing is not valid. It needs an amount, created and counterparty_alias to be valid, but received: $this"
+        "PaymentListing is not valid. It needs an amount, created and counterparty_alias to be valid, " +
+            "but received: $this"
     }
     val amount = amount!!
     val created = created!!
@@ -208,11 +243,14 @@ private fun Amount.toTransactionType(): TransactionType =
     }
 
 private fun LabelMonetaryAccount?.toDomain(): CounterParty {
-    require(isValid()) { "LabelMonetaryAccount is not valid. It needs a display_name and iban to be valid, but received: $this" }
+    require(isValid()) {
+        "LabelMonetaryAccount is not valid. It needs a display_name and iban to be valid, " +
+            "but received: $this"
+    }
     return CounterParty(
         iban = this!!.iban,
         displayName = this.display_name!!,
     )
 }
 
-private fun LabelMonetaryAccount?.isValid() = this != null && this.display_name != null
+private fun LabelMonetaryAccount?.isValid() = this != null && this.display_name != null && this.iban != null
